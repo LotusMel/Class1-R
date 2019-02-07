1242bda0a3431246efe31e005a404c4a44627f24
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-02-07 04:06:41 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class1_R
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            if (name == "Amar")
+            {
+                Console.WriteLine("Hello, " + name + ". I like your shoes");
+            }
+            else if (name == "Brandy")
+            {
+                Console.WriteLine("Hello, " + name + ". You seem like a cool person");
+            }
+            else if (name == "Emma")
+            {
+                Console.WriteLine("Hi, Baby Girl!");
+            }
+            else
+            {
+                Console.WriteLine("Hello ," + name + ".");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
